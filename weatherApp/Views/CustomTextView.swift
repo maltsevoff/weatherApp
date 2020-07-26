7f1ecb20982019b9d89971c996910411962c7c25
maltsevoff <alexmalczev@gmail.com>
2020-07-26 21:00:51 +0300

    added textview

---
+++ weatherApp/Views/CustomTextView.swift
@@ -0,0 +1,21 @@
+//
+//  CustomTextInput.swift
+//  weatherApp
+//
+//  Created by Aleksandr Maltsev on 26.07.2020.
+//  Copyright © 2020 Aleksandr Maltsev. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextView: UITextView {
+	
+	required init?(coder: NSCoder) {
+		super.init(coder: coder)
+		
+		layer.cornerRadius = 10
+		layer.masksToBounds = true
+		backgroundColor = UIColor.lightGray.withAlphaComponent(0.8)
+		isHidden = true
+	}
+}
