c4ee7d0b15810492d32ab9b61dca01b3bf7cbfef
maltsevoff <alexmalczev@gmail.com>
2020-07-26 10:56:53 +0300

    init

---
+++ weatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weatherApp
+//
+//  Created by Aleksandr Maltsev on 26.07.2020.
+//  Copyright © 2020 Aleksandr Maltsev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
